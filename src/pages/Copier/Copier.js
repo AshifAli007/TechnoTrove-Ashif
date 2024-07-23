a18804215218fe0718626760595e47f86f7bfa41
Ashif <ashif@Jarvis.local>
2024-07-23 02:13:15 -0400

    Finished the clopier

--- src/pages/Copier/Copier.js
+++ src/pages/Copier/Copier.js
@@ -1,8 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
 import "./Copier.scss"
+import { useDropzone } from 'react-dropzone';
+import { FaCloudUploadAlt } from "react-icons/fa";
+import { BsFileEarmarkText } from "react-icons/bs";
+import { BsFileEarmarkArrowDown } from "react-icons/bs";
+import { GiHummingbird } from "react-icons/gi";
+
+
+
+
+
+
+
 
 const Copier = () => {
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+        onDrop: (files) => {
+            console.log(files, 'dropzone files')
+            let selectedFile = files[0]
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                sendFileMessage(event.target.result);
+            };
+            reader.readAsArrayBuffer(selectedFile);
+        }
+    });
+    const files = acceptedFiles.map(file => (
+        <div key={file.path}>
+            {file.path} - {file.size} bytes
+        </div>
+    ));
     const [data, setData] = useState('');
     const [receivedData, setReceivedData] = useState('');
     // const [file, setFile] = useState(null);
@@ -11,8 +39,7 @@ const Copier = () => {
     const dataServer = "wss://copier-ws-production.up.railway.app";
     const fileServer = "wss://lean-secret-spruce.glitch.me";
 
-    // const { sendMessage: sendTextMessage, lastMessage, readyState: dataReadyState } = useWebSocket(dataServer, {
-    const { sendMessage: sendTextMessage, lastMessage } = useWebSocket(dataServer, {
+    const { sendMessage: sendTextMessage, lastMessage, readyState: dataReadyState } = useWebSocket(dataServer, {
         shouldReconnect: () => true,
     });
 
@@ -64,13 +91,13 @@ const Copier = () => {
         reader.readAsArrayBuffer(selectedFile);
     };
 
-    // const dataConnectionStatus = {
-    //     [ReadyState.CONNECTING]: 'Connecting',
-    //     [ReadyState.OPEN]: 'Open',
-    //     [ReadyState.CLOSING]: 'Closing',
-    //     [ReadyState.CLOSED]: 'Closed',
-    //     [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-    // }[dataReadyState];
+    const dataConnectionStatus = {
+        [ReadyState.CONNECTING]: 'Connecting',
+        [ReadyState.OPEN]: 'Open',
+        [ReadyState.CLOSING]: 'Closing',
+        [ReadyState.CLOSED]: 'Closed',
+        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    }[dataReadyState];
 
     const fileConnectionStatus = {
         [ReadyState.CONNECTING]: 'Connecting',
@@ -83,40 +110,82 @@ const Copier = () => {
     return (
         <div className='container'>
             <div className='text-sync'>
+
                 <textarea
                     value={data}
                     onChange={handleChange}
                     placeholder="Type or paste your data here"
                     rows="10"
-                    cols="70"
+                    cols="60"
                     className='send custom-scrollbar'
                 />
-                <textarea
-                    value={receivedData}
-                    onChange={handleChange}
-                    placeholder="Type or paste your data here"
-                    rows="10"
-                    cols="70"
-                    className='receive custom-scrollbar'
-                />
+                <div>
+                    <textarea
+                        value={receivedData}
+                        onChange={handleChange}
+                        placeholder="Type or paste your data here"
+                        rows="10"
+                        cols="60"
+                        className='receive custom-scrollbar'
+                        style={{ height: '100%' }}
+                    />
+                </div>
+                <GiHummingbird size={35} style={{ position: 'relative', top: '5px', right: '15px', color: dataConnectionStatus !== 'Closed' && dataConnectionStatus !== 'Closing' ? 'rgb(111, 211, 131)' : 'red' }} />
+
+
+
             </div>
             <div className='file-sync'>
-                <label for="file-upload" class="dragndrop">
-                    Custom Upload
-                </label>
-                <input id="file-upload" type="file" onChange={handleFileChange} />
+                <div className='dropbox'>
+                    <div className='zone' onChange={handleFileChange} {...getRootProps({ className: 'dropzone' })}>
+                        <input {...getInputProps()} />
+                        <p>Drag 'n' drop some files here, or click to select files</p>
+                        <FaCloudUploadAlt className='upload-icon' size={80} />
+                        <p style={{ paddingTop: '0px' }}>{files}</p>
+                    </div>
 
-                {console.log(receivedFile, 'fdfd')}
-                <p>Received File: {receivedFile ? <a href={receivedFile} download="received_file">Download</a> : 'No file received'}</p>
-                <p>File Connection Status: {fileConnectionStatus}</p>
+
+                </div>
+                <div className='reciever'>
+                    <GiHummingbird size={35} style={{ position: 'absolute', top: '5px', right: '5px', color: fileConnectionStatus !== 'Closed' && fileConnectionStatus !== 'Closing' ? 'rgb(111, 211, 131)' : 'red' }} />
+
+
+                    {receivedFile ?
+                        <div className='download-zone'>
+                            <div>
+                                <a href={receivedFile} download="received_file">
+                                    <BsFileEarmarkArrowDown
+                                        size={100}
+                                        style={{
+                                            color: 'rgb(111 211 131)',
+                                            boxShadow: '0px 0px 30px rgb(186 238 229)',
+                                            borderRadius: '10px',
+                                            cursor: 'pointer'
+                                        }}
+                                    />
+                                </a>
+                            </div>
+                            <p>
+                                File recieved, click above to download
+                            </p>
+                        </div> :
+                        <div className='download-zone'>
+                            <div>
+                                <BsFileEarmarkText size={100} style={{ color: 'grey' }} />
+                            </div>
+                            <p>
+                                No file recieved
+                            </p>
+
+                        </div>
+                    }
+                </div>
+
             </div>
 
 
             {/* <p>Data Connection Status: {dataConnectionStatus}</p> */}
-            {/* <div {...getRootProps({className: 'dropzone'})}>
-                <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            </div> */}
+
 
         </div>
     );
